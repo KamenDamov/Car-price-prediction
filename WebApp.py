99969120d7687dc11f81cf601827098a5de4f8a6
KamenDamov <kamendamov123@gmail.com>
2022-07-03 23:33:53 -0400

    push

--- WebApp.py
+++ WebApp.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 
+#Main function to predict price
 def predict_price(input): 
     pass
     #model = pickle.load(open('model.pkl','rb'))
@@ -12,9 +13,26 @@ car = gr.Dropdown(label = "Car brand", choices=['Alfa-Romero', 'Audi', 'BMW', 'C
        'Nissan', 'Peugeot', 'Plymouth', 'Porsche', 'Renault',
        'Saab', 'Subaru', 'Toyota', 'Volkswagen', 'Volvo'])
 
+fuel = gr.Radio(label = "Fuel Type", choices = ['Gas', 'Diesel'])
+
+aspiration = gr.Radio(label = "Aspiration type", choices = ["Standard", "Turbo"])
+
+door_number = gr.Radio(label = "Number of doors", choices = ["Two", "Four"])
+
+car_body = gr.Dropdown(label ="Car body type", choices = ['convertible', 'hatchback', 'sedan', 'wagon', 'hardtop'])
+
 output = gr.Textbox()
 
-app = gr.Interface(title="Predict the price of your car", fn=predict_price,inputs=[car],outputs=output)
+app = gr.Interface(title="Predict the price of your car", 
+                    fn=predict_price,
+                    inputs=[car,
+                            fuel,
+                            aspiration,
+                            door_number,
+                            car_body,
+                            
+                            ],
+                    outputs=output)
 
 app.launch()
 #print(model)
